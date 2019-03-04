95701ba53ccad3bb3e8289e254e84cd97c57f880
nico <349036010@qq.com>
2019-03-04 23:35:10 +0800

    新增角色查询功能

--- aquaman-security-os/src/main/java/com/aquaman/security/admin/service/IRoleService.java
+++ aquaman-security-os/src/main/java/com/aquaman/security/admin/service/IRoleService.java
@@ -1,25 +1,1 @@
-package com.aquaman.security.admin.service;
-
-import com.aquaman.security.admin.entity.domain.Role;
-import com.aquaman.security.admin.entity.query.RoleQuery;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author wei wang
- * @since 2019-03-04
- */
-public interface IRoleService extends IService<Role> {
-
-    /**
-     * 角色分页查询
-     * @param query
-     * @return
-     */
-    IPage<Role> page(RoleQuery query);
-
-}
+package com.aquaman.security.admin.service;import com.aquaman.security.admin.entity.domain.Role;import com.aquaman.security.admin.entity.query.RoleQuery;import com.baomidou.mybatisplus.core.metadata.IPage;import com.baomidou.mybatisplus.extension.service.IService;/** * <p> *  服务类 * </p> * * @author wei wang * @since 2019-03-04 */public interface IRoleService extends IService<Role> {    /**     * 角色分页查询     * @param query     * @return     */    IPage<Role> page(RoleQuery query);}
