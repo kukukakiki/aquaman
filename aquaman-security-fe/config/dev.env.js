f62876402c7b29aa22f1818e52f24321ba629f05
nico <349036010@qq.com>
2019-03-15 23:52:27 +0800

    移除fe工程

--- aquaman-security-fe/config/dev.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  NODE_ENV: '"development"',
-  ENV_CONFIG: '"dev"',
-  BASE_API: '"http://aquaman.leerzoom.com:8001"',
-  HEANER_AUTHO_BASIC: '"Basic YXF1YW1hbjphcXVhbWFuMjAxOQ=="'
-}
